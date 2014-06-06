3627d8738ba936495e049b87dfa731179944b2d7
unixninja92 <charlesvt@gmail.com>
2014-06-06 14:58:47 -0400

    Started adding different versions of crypto used for CryptBucket

--- src/freenet/crypt/CryptBucket.java
+++ src/freenet/crypt/CryptBucket.java
@@ -3,5 +3,9 @@ package freenet.crypt;
 import freenet.support.api.Bucket;
 
 public class CryptBucket implements Bucket {
-	
+	public static final byte ALGO_AES_PCFB_256_SHA256 = 2;
+    public static final byte ALGO_AES_CTR_256_SHA256 = 3;
+    public static final byte ALGO_CHACHA = 4;
+	private byte cryptoAlgorithm;
+    private boolean readOnly;
 }
