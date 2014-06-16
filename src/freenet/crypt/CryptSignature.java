0edd5f755e07ea6f6996b60b92203eaf100345d1
unixninja92 <charlesvt@gmail.com>
2014-06-15 20:00:59 -0400

    Added constructor to CryptSig for getting keys from SimpleFieldSets

--- src/freenet/crypt/CryptSignature.java
+++ src/freenet/crypt/CryptSignature.java
@@ -18,8 +18,13 @@ import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
+import freenet.node.FSParseException;
 import freenet.support.Base64;
 import freenet.support.Logger;
 import freenet.support.SimpleFieldSet;
@@ -27,12 +32,14 @@ import net.i2p.util.NativeBigInteger;
 
 public class CryptSignature{
 	private static final SigType defaultType = PreferredAlgorithms.preferredSignature;
+	private SigType type;
 	private KeyPair keys;
 	private Signature sig;
 	private KeyPairGenerator kg;
 
 	public CryptSignature(){
 		try {
+			type = defaultType;
 			kg = KeyPairGenerator.getInstance(PreferredAlgorithms.preferredKeyGen, 
 					PreferredAlgorithms.keyGenProvider);
 			kg.initialize(defaultType.getSpec());
@@ -45,6 +52,34 @@ public class CryptSignature{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public CryptSignature(SimpleFieldSet sfs, SigType type) throws FSParseException{
+		byte[] pub = null;
+        byte[] pri = null;
+        try {
+            KeyFactory kf = KeyFactory.getInstance(PreferredAlgorithms.preferredKeyGen, PreferredAlgorithms.keyGenProvider);
+            
+            pub = Base64.decode(sfs.get("pub"));
+            if (pub.length > type.modulusSize)
+                throw new InvalidKeyException();
+            X509EncodedKeySpec xks = new X509EncodedKeySpec(pub);
+            ECPublicKey pubK = (ECPublicKey)kf.generatePublic(xks);
+            
+            pri = Base64.decode(sfs.get("pri"));
+            PKCS8EncodedKeySpec pks = new PKCS8EncodedKeySpec(pri);
+            ECPrivateKey privK = (ECPrivateKey) kf.generatePrivate(pks);
+            
+            keys = new KeyPair(pubK, privK);
+        }  catch (NoSuchAlgorithmException e) {
+            Logger.error(ECDSA.class, "NoSuchAlgorithmException : "+e.getMessage(),e);
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            Logger.error(ECDSA.class, "InvalidKeySpecException : "+e.getMessage(), e);
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new FSParseException(e);
+        }
 	}
 	
 	public void addByte(byte input){
@@ -87,26 +122,47 @@ public class CryptSignature{
         byte[] result = null;
         try{
         	while(true) {
-//        		sig.initSign(keys.getPrivate());
+        		sig.initSign(keys.getPrivate());
         		for(byte[] b: data){
         			addBytes(b);
         		}
         		result = sig.sign();
         		// It's a DER encoded signature, most sigs will fit in N bytes
                 // If it doesn't let's re-sign.
-                if(result.length <= defaultType.maxSigSize)
+                if(result.length <= type.maxSigSize)
                 	break;
                 else
-                	Logger.error(this, "DER encoded signature used "+result.length+" bytes, more than expected "+defaultType.maxSigSize+" - re-signing...");
+                	Logger.error(this, "DER encoded signature used "+result.length+" bytes, more than expected "+type.maxSigSize+" - re-signing...");
         	}
         } catch(SignatureException e){
         	Logger.error(CryptSignature.class, "SignatureException : "+e.getMessage(),e);
         	e.printStackTrace();
-//        } catch (InvalidKeyException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
         return result;
+    }
+	
+	/**
+     * Sign data and return a fixed size signature. The data does not need to be hashed, the 
+     * signing code will handle that for us, using an algorithm appropriate for the keysize.
+     * @return A zero padded DER signature (maxSigSize). Space Inefficient but constant-size.
+     */
+    public byte[] signToNetworkFormat(byte[]... data) {
+        byte[] plainsig = sign(data);
+        int targetLength = type.maxSigSize;
+
+        if(plainsig.length != targetLength) {
+            byte[] newData = new byte[targetLength];
+            if(plainsig.length < targetLength) {
+                System.arraycopy(plainsig, 0, newData, 0, plainsig.length);
+            } else {
+                throw new IllegalStateException("Too long!");
+            }
+            plainsig = newData;
+        }
+        return plainsig;
     }
 
 	public boolean verify(byte[] signature){
@@ -152,7 +208,7 @@ public class CryptSignature{
         fsCurve.putSingle("pub", Base64.encode(keys.getPublic().getEncoded()));
         if(includePrivate)
             fsCurve.putSingle("pri", Base64.encode(keys.getPrivate().getEncoded()));
-        fs.put(defaultType.name(), fsCurve);
+        fs.put(type.name(), fsCurve);
         return fs;
     }
 }
