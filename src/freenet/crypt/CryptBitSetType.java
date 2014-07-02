ba548298fced5cd7878a9c134271009e71dd40bc
unixninja92 <charlesvt@gmail.com>
2014-07-02 18:27:06 -0400

    Created CryptBitSet and CryptBitSetType classes. Coppied over non AEAD algs from CryptBucketType to CryptBitSetType. in CryptBucketType depreciated AEADAESOCBDraft00.

---
+++ src/freenet/crypt/CryptBitSetType.java
@@ -0,0 +1,41 @@
+package freenet.crypt;
+
+
+public enum CryptBitSetType {
+	RijndaelECB(1, KeyType.Rijndael256),
+	RigndaelECB(2, KeyType.Rijndael256, 128),
+	RijndaelCTR(4, KeyType.Rijndael256),
+	RijndaelPCFB(8, KeyType.Rijndael256),
+	AESCTR(16, "AES/CTR/NOPADDING", KeyType.AES256),
+	ChaCha(32, KeyType.ChaCha);
+	
+	public final int bitmask;
+	public final int blockSize;
+	public final String algName;
+	public final String cipherName;
+	public final KeyType keyType;
+	
+	CryptBitSetType(int bitmask, KeyType keyType){
+	this.bitmask = bitmask;
+	this.keyType = keyType;
+	this.cipherName = keyType.alg;
+	this.blockSize = keyType.keySize;
+	algName = name();
+	}
+	
+	CryptBitSetType(int bitmask, KeyType keyType, int blockSize){
+		this.bitmask = bitmask;
+		this.keyType = keyType;
+		this.cipherName = keyType.alg;
+		this.blockSize = blockSize;
+		algName = name();
+	}
+	
+	CryptBitSetType(int bitmask, String algName, KeyType keyType){
+		this.bitmask = bitmask;
+		this.cipherName = keyType.alg;
+		this.blockSize = keyType.keySize;
+		this.algName = algName;
+		this.keyType = keyType;
+	}
+}
--- src/freenet/crypt/KeyType.java
+++ src/freenet/crypt/KeyType.java
@@ -1,11 +1,14 @@
 package freenet.crypt;
 
 public enum KeyType {
+	Rijndael128("Rijndael", 128),
+	Rijndael256("Rijndael", 256),
 	AES128("AES", 128),
 	AES256("AES", 256),
 	HMACSHA1("HMACSHA1"), 
 	HMACSHA256("HMACSHA256"),
-	POLY1305("POLY1305-AES");
+	POLY1305("POLY1305-AES"),
+	ChaCha("CHACHA", 128);
 	
 	public final String alg;
 	public final int keySize;
