3888f01ce60f248bbcf189f9a6cb68e9158b4178
unixninja92 <charlesvt@gmail.com>
2014-07-02 18:27:06 -0400

    Created CryptBitSet and CryptBitSetType classes. Coppied over non AEAD algs from CryptBucketType to CryptBitSetType. in CryptBucketType depreciated AEADAESOCBDraft00.

---
+++ src/freenet/crypt/CryptBitSet.java
@@ -0,0 +1,8 @@
+package freenet.crypt;
+
+import java.util.BitSet;
+
+public class CryptBitSet {
+	public final static CryptBitSetType defaultType = CryptBitSetType.ChaCha;
+	private BitSet data;
+}
--- src/freenet/crypt/CryptBucketType.java
+++ src/freenet/crypt/CryptBucketType.java
@@ -4,11 +4,7 @@
 package freenet.crypt;
 
 public enum CryptBucketType {
-//	RijndaelECB(1, "Rijndael", 256),
-//	RigndaelECB(2, "Rijndael", 256, 128),
-//	RijndaelCTR(4, "Rijndael", 256),
-//	RijndaelPCFB(8, "Rijndael", 256),
-//	AESCTR(16, "AES", "AES/CTR/NOPADDING", KeyType.AES256),
+	@Deprecated
 	AEADAESOCBDraft00(32, "AES", KeyType.AES128),
 	AEADAESOCB(64, "AES", KeyType.AES128);
 	
@@ -19,15 +15,6 @@ public enum CryptBucketType {
 	public final String cipherName;
 	public final KeyType keyType;
 	
-//	CryptBucketType(int bitmask, String cipherName, int keySize){
-//		this.bitmask = bitmask;
-//		this.cipherName = cipherName;
-//		this.keySize = keySize;
-//		this.blockSize = keySize;
-//		algName = name();
-//		keyType = null;
-//	}
-	
 	CryptBucketType(int bitmask, String cipherName, KeyType keyType){
 		this.bitmask = bitmask;
 		this.cipherName = cipherName;
@@ -36,23 +23,5 @@ public enum CryptBucketType {
 		algName = name();
 		this.keyType = keyType;
 	}
-	
-//	CryptBucketType(int bitmask, String cipherName, int keySize, int blockSize){
-//		this.bitmask = bitmask;
-//		this.cipherName = cipherName;
-//		this.keySize = keySize;
-//		this.blockSize = blockSize;
-//		algName = name();
-//		keyType = null;
-//	}
-	
-//	CryptBucketType(int bitmask, String cipherName, String algName, KeyType keyType){
-//		this.bitmask = bitmask;
-//		this.cipherName = cipherName;
-//		this.keySize = keyType.keySize;
-//		this.blockSize = keySize;
-//		this.algName = algName;
-//		this.keyType = keyType;
-//	}
 	
 }
