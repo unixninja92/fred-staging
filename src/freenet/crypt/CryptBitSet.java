2a0d7291581451dd01d1c2220cce34a6c53037a6
unixninja92 <charlesvt@gmail.com>
2014-07-03 15:21:13 -0400

    Started writing documentation for CryptBitSet

--- src/freenet/crypt/CryptBitSet.java
+++ src/freenet/crypt/CryptBitSet.java
@@ -15,6 +15,10 @@ import javax.crypto.SecretKey;
 
 import freenet.crypt.ciphers.Rijndael;
 
+/**
+ * CryptBitSet will encrypt and decrypt both byte[]s and BitSets with a specified
+ * algorithm, key, and iv. 
+*/
 public class CryptBitSet {
 	public final static CryptBitSetType defaultType = CryptBitSetType.ChaCha;
 	private CryptBitSetType type;
@@ -25,6 +29,12 @@ public class CryptBitSet {
 	private PCFBMode pcfb;
 	private byte[] iv;
 	
+	/**
+	 * Creates an instance of CryptBitSet that will be able to encrypt and decrypt 
+	 * sets of bytes using the algorithm type with the specified key.
+	 * @param type The symmetric algorithm, mode, and key and block size to use
+	 * @param key The key that will be used for encryption
+	 */
 	public CryptBitSet(CryptBitSetType type, SecretKey key){
 		this.type = type;
 
@@ -46,6 +56,14 @@ public class CryptBitSet {
 		this(type, KeyUtils.getSecretKey(key, type.keyType));
 	}
 	
+	
+	/**
+	 * Creates an instance of CryptBitSet that will be able to encrypt and decrypt 
+	 * sets of bytes using the algorithm type with the specified key and iv.
+	 * @param type
+	 * @param key
+	 * @param iv
+	 */
 	public CryptBitSet(CryptBitSetType type, SecretKey key, byte[] iv) {
 		this.type = type;
 		this.key = key;
@@ -66,6 +84,16 @@ public class CryptBitSet {
 		this(type, KeyUtils.getSecretKey(key, type.keyType), iv);
 	}
 
+	/**
+	 * Encrypts or decrypts a specified section of input.
+	 * @param mode Sets mode to either encrypt or decrypt. 0 for decryption,
+	 * 1 for encryption.
+	 * @param input The byte[] to be processes(either encrypted or decrypted)
+	 * @param offset The position in the byte[] to start processing at
+	 * @param len How many more bytes to process in the array past offset
+	 * @return Depending on the value of mode will either return an encrypted
+	 * or decrypted version of the selected portion of the byte[] input
+	 */
 	private byte[] processesBytes(int mode, byte[] input, int offset, int len){
 		try {
 			if(type.cipherName == "Rijndael"){
@@ -111,9 +139,17 @@ public class CryptBitSet {
 		return null;
 	}
 	
+	/**
+	 * Encrypts the specified section of input
+	 * @param input The bytes to be encrypted
+	 * @param offset The position of input to start encrypting at
+	 * @param len The number of bytes after offset to encrypt
+	 * @return Returns byte[] input with the specified section encrypted
+	 */
 	public byte[] encrypt(byte[] input, int offset, int len){
 		return processesBytes(1, input, offset, len);
 	}
+	
 	
 	public byte[] encrypt(byte[] input){
 		return encrypt(input, 0, input.length);
