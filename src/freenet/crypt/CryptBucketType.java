103693f3ca5e732e5a49597aeb14d8a3ebc7b1c3
unixninja92 <charlesvt@gmail.com>
2014-07-03 16:25:36 -0400

    Removed extra variables not needed anymore in CryptBucketType. Also moved the choice of AES engines to CryptBucketType.

--- src/freenet/crypt/CryptBucketType.java
+++ src/freenet/crypt/CryptBucketType.java
@@ -4,27 +4,29 @@
 package freenet.crypt;
 
 import org.bouncycastle.crypto.BlockCipher;
+import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.engines.AESLightEngine;
 import org.bouncycastle.crypto.modes.AEADBlockCipher;
 import org.bouncycastle.crypto.modes.OCBBlockCipher;
 
 public enum CryptBucketType {
 	@Deprecated
-	AEADAESOCBDraft00(32, "AES", KeyType.AES128, 16),
-	AEADAESOCB(64, "AES", KeyType.AES128, 15);
+	AEADAESOCBDraft00(1, KeyType.AES128, 16),
+	AEADAESOCB(2, KeyType.AES128, 15);
 	
 	public final int bitmask;
-	public final String cipherName;
 	public final KeyType keyType;
 	public final int nonceSize;
 	
-	CryptBucketType(int bitmask, String cipherName, KeyType keyType, int nonceSize){
+	CryptBucketType(int bitmask, KeyType keyType, int nonceSize){
 		this.bitmask = bitmask;
-		this.cipherName = cipherName;
 		this.keyType = keyType;
 		this.nonceSize = nonceSize;
 	}
 	
-	public AEADBlockCipher getBlockCipher(BlockCipher hashCipher, BlockCipher mainCipher){
+	public AEADBlockCipher getBlockCipher(){
+		BlockCipher hashCipher = new AESLightEngine();
+		BlockCipher mainCipher = new AESFastEngine();
 		if(nonceSize == 16){
 			return new OldOCBBlockCipher(hashCipher, mainCipher);
 		}
