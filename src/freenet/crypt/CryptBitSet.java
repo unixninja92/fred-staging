548be29e47d483a366c544d7f1dbf2e0211921f7
unixninja92 <charlesvt@gmail.com>
2014-07-03 15:31:07 -0400

    Documentation of CryptBitSet mostly done

--- src/freenet/crypt/CryptBitSet.java
+++ src/freenet/crypt/CryptBitSet.java
@@ -17,14 +17,17 @@ import freenet.crypt.ciphers.Rijndael;
 
 /**
  * CryptBitSet will encrypt and decrypt both byte[]s and BitSets with a specified
- * algorithm, key, and iv. 
+ * algorithm, key, and also an iv if the algorithm requires one. 
 */
 public class CryptBitSet {
 	public final static CryptBitSetType defaultType = CryptBitSetType.ChaCha;
 	private CryptBitSetType type;
-	private Cipher cipher;
 	private SecretKey key;
+
+	//Used for AES and ChaCha ciphers
+	private Cipher cipher;
 	
+	//These variables are used with Rijndael ciphers
 	private BlockCipher blockCipher;
 	private PCFBMode pcfb;
 	private byte[] iv;
@@ -150,23 +153,49 @@ public class CryptBitSet {
 		return processesBytes(1, input, offset, len);
 	}
 	
-	
+	/**
+	 * Encrypts the entire byte[] input
+	 * @param input The byte[] to be encrypted
+	 * @return The encrypted byte[]
+	 */
 	public byte[] encrypt(byte[] input){
 		return encrypt(input, 0, input.length);
 	}
 	
+	/**
+	 * Encrypts the BitSet input
+	 * @param input The BitSet to encrypt
+	 * @return The encrypted BitSet
+	 */
 	public BitSet encrypt(BitSet input){
 		return BitSet.valueOf(encrypt(input.toByteArray()));
 	}
 	
+	/**
+	 * Decrypts the specified section of input
+	 * @param input The bytes to be decrypted
+	 * @param offset The position of input to start decrypting at
+	 * @param len The number of bytes after offset to decrypt
+	 * @return Returns byte[] input with the specified section decrypted
+	 */
 	public byte[] decrypt(byte[] input, int offset, int len){
 		return processesBytes(0, input, offset, len);
 	}
 	
+	/**
+	 * Decrypts the entire byte[] input
+	 * @param input The byte[] to be decrypted
+	 * @return The decrypted byte[]
+	 */
 	public byte[] decrypt(byte[] input){
 		return decrypt(input, 0, input.length);
 	}
 	
+	/**
+	 * Decrypts the BitSet input
+	 * @param input The BitSet to decrypt
+	 * @return The decrypted BitSet
+	 */
 	public BitSet decrypt(BitSet input){
 		return BitSet.valueOf(decrypt(input.toByteArray()));
 	}
