f45c57fe904687248942e1124ea74599bcef379d
unixninja92 <charlesvt@gmail.com>
2014-07-07 15:55:30 -0400

    Finished writing unit tests for Hash

--- test/freenet/crypt/HashTest.java
+++ test/freenet/crypt/HashTest.java
@@ -3,6 +3,8 @@ package freenet.crypt;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import net.i2p.util.NativeBigInteger;
+
 import org.bouncycastle.util.encoders.Hex;
 
 import junit.framework.TestCase;
@@ -38,16 +40,69 @@ public class HashTest extends TestCase {
 			}
 			
 			byte[] abcResult = hash.getHash(abc);
-			byte[] expectedABCResult = Hex.decode(abcVectors[i][1]);
+			byte[] expectedABCResult = getABCByteArray(i);
 			
 			assertEquals(MessageDigest.isEqual(abcResult, expectedABCResult), true);
 		}
+	}
+	
+	//tests getHashResult() as well
+	public void testGetHashResultHashResultByteArray() {
+		HashType type = HashType.SHA256;
+		byte[] hashResult = getABCByteArray(2);
+		HashResult hash2 = new HashResult(type, hashResult);
+		
+		Hash hash = new Hash(type);
+		HashResult hash1 = hash.getHashResult(abc);
+		
+		assertEquals(Hash.verify(hash1, hash2), true);
+	}
+	
+	public void testGetHashHex(){
+		Hash hash = new Hash(HashType.SHA256);
+		hash.addBytes(abc);
+		String hexHash = hash.getHexHash();
+		
+		assertEquals(abcVectors[2][1], hexHash);
+	}
+	
+	public void testGetNativeBIgIntegerHashByteArrayArray(){
+		Hash hash = new Hash(HashType.SHA256);
+		NativeBigInteger abcVector = new NativeBigInteger(1, getABCByteArray(2));
+		NativeBigInteger result = hash.getNativeBigIntegerHash(abc);
+		assertEquals(result, abcVector);
+		
+	}
+	
+	public void testAddByteByte(){
+		byte[] message = "hello world".getBytes();
+		Hash hash = new Hash(HashType.SHA256);
+		byte[] result = hash.getHash(message);
+		
+        for (int i = 0; i < message.length; i++)
+        {
+            hash.addByte(message[i]);
+        }
+        byte[] result2 = hash.getHash();
+        
+        assertEquals(MessageDigest.isEqual(result, result2), true);	
+	}
+	
+	public void testAddByteByteArrayIntInt(){
+		byte[] message = "hello world".getBytes();
+		Hash hash = new Hash(HashType.SHA256);
+		byte[] result = hash.getHash(message);
+		
+		hash.addBytes(message, 0, message.length/2);
+		hash.addBytes(message, message.length/2, message.length-message.length/2);
+		byte[] result2 = hash.getHash();
+        assertEquals(MessageDigest.isEqual(result, result2), true);	
 	}
 
 	public void testVerifyByteArrayByteArray() {
 		for(int i = 0; i<typeVector.length; i++){
 			Hash hash = new Hash(typeVector[i]);
-			boolean verified = hash.verify(Hex.decode(abcVectors[i][1]), abc);
+			boolean verified = hash.verify(getABCByteArray(i), abc);
 			
 			assertEquals(verified, true);
 		}
@@ -55,7 +110,7 @@ public class HashTest extends TestCase {
 
 	public void testVerifyHashResultByteArray() {
 		HashType type = HashType.SHA256;
-		byte[] hashResult = Hex.decode(abcVectors[3][1]);
+		byte[] hashResult = getABCByteArray(2);
 		HashResult hash1 = new HashResult(type, hashResult);
 		
 		assertEquals(Hash.verify(hash1, hashResult), true);
@@ -63,11 +118,15 @@ public class HashTest extends TestCase {
 	
 	public void testVerifyHashResultHashResult() {
 		HashType type = HashType.SHA256;
-		byte[] hashResult = Hex.decode(abcVectors[3][1]);
+		byte[] hashResult = getABCByteArray(2);
 		HashResult hash1 = new HashResult(type, hashResult);
 		HashResult hash2 = new HashResult(type, hashResult);
 		
 		assertEquals(Hash.verify(hash1, hash2), true);
+	}
+	
+	private byte[] getABCByteArray(int alg){
+		return Hex.decode(abcVectors[alg][1]);
 	}
 
 }
