a35d2340192720a57da4da0718a6a12a1320ca8d
unixninja92 <charlesvt@gmail.com>
2014-07-10 13:43:03 -0400

    Refactored KeyUtilsTest to KeyGenTest

---
+++ test/freenet/crypt/KeyGenTest.java
@@ -0,0 +1,47 @@
+package freenet.crypt;
+
+import junit.framework.TestCase;
+
+public class KeyGenTest extends TestCase {
+
+	public void testGenKeyPair() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPublicKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPublicKeyPair() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetKeyPairByteArrayByteArray() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetKeyPairPublicKeyPrivateKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testGenSecretKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSecretKey() {
+		fail("Not yet implemented");
+	}
+
+	public void testGenNonce() {
+		fail("Not yet implemented");
+	}
+
+	public void testGenIV() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetIvParameterSpec() {
+		fail("Not yet implemented");
+	}
+
+}
