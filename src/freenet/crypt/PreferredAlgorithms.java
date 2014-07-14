3018dbecd4d6bf3e360069ffd65e88a622c66e12
unixninja92 <charlesvt@gmail.com>
2014-07-14 19:01:43 -0400

    Removed imports from PreferredAlgs

--- src/freenet/crypt/PreferredAlgorithms.java
+++ src/freenet/crypt/PreferredAlgorithms.java
@@ -4,38 +4,6 @@
 
 package freenet.crypt;
 
-import java.security.GeneralSecurityException;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.Provider;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Signature;
-import java.security.spec.ECGenParameterSpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-
-import freenet.crypt.JceLoader;
-import freenet.node.Node;
-import freenet.node.NodeStarter;
-import freenet.support.Logger;
-
 @Deprecated 
 public final class PreferredAlgorithms{
 
