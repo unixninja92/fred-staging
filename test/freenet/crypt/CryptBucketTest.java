1fe78f470ed6fcafc8cd72d16eddc5a16088efbe
unixninja92 <charlesvt@gmail.com>
2014-08-06 15:45:07 -0400

    Created new test classes

---
+++ test/freenet/crypt/CryptBucketTest.java
@@ -0,0 +1,89 @@
+package freenet.crypt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CryptBucketTest {
+
+    @Test
+    public void testDecrypt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddByte() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddBytesByteArrayArray() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddBytesByteArrayIntInt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEncrypt() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEncryptBytes() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetOutputStream() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInputStream() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSize() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsReadOnly() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetReadOnly() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFree() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStoreTo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRemoveFrom() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShadow() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ test/freenet/crypt/KeyExchangeTest.java
@@ -0,0 +1,24 @@
+package freenet.crypt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KeyExchangeTest {
+
+    @Test
+    public void testGetPublicKeyNetworkFormat() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSharedSecrect() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPublicKey() {
+        fail("Not yet implemented");
+    }
+
+}
